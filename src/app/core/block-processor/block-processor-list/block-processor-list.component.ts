8b0ef23939597ff05205503ec6e9fc4b0a442a42
NadimMetacaps <161430554+NadimMetacaps@users.noreply.github.com>
2024-05-12 17:26:35 +0530

    blocks-processor , customer, supplier changes in view component

--- src/app/core/block-processor/block-processor-list/block-processor-list.component.ts
+++ src/app/core/block-processor/block-processor-list/block-processor-list.component.ts
@@ -54,6 +54,10 @@ export class BlockProcessorListComponent {
     console.log(id);
     this.router.navigate(["/block-processor/edit-block-processor/" + id]);
   }
+  vewBlockProcessor(id) {
+    console.log(id);
+    this.router.navigate(["/block-processor/view-block-processor/" + id]);
+  }
 
   deleteBlockProcessor(Id: any) {
     this.blockProcessorId = Id;
