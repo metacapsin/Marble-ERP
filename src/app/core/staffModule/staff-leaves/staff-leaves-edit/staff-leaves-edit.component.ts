b6e3f5a6c0ed9f2d2fe0054734b5afad71f44c66
AdnanhussainMetacaps <161420107+AdnanhussainMetacaps@users.noreply.github.com>
2024-05-29 13:44:53 +0530

    changes for leave request

--- src/app/core/staffModule/staff-leaves/staff-leaves-edit/staff-leaves-edit.component.ts
+++ src/app/core/staffModule/staff-leaves/staff-leaves-edit/staff-leaves-edit.component.ts
@@ -1,12 +1,123 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from "@angular/router";
+import { routes } from "src/app/shared/routes/routes";
+import { staffLeavesService } from '../staff-leaves.service';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { staffService } from '../../staff/staff-service.service';
 @Component({
   selector: 'app-staff-leaves-edit',
-  standalone: true,
-  imports: [],
   templateUrl: './staff-leaves-edit.component.html',
-  styleUrl: './staff-leaves-edit.component.scss'
+  styleUrl: './staff-leaves-edit.component.scss',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SharedModule,
+    CalendarModule,
+    DropdownModule,
+    ToastModule
+  ],
+  providers:[MessageService]
 })
 export class StaffLeavesEditComponent {
+  public routes = routes;
+  public selectedValue !: string ;
+  editLeaveForm! : FormGroup
+  leaveId : any;
+  leaveDataById = [];
+  employeeList = [];
+  LeaveData = [
+  {value: 'Select Leave Type'},
+  {value: 'Medical Leave'},
+  {value: 'Casual Leave'},
+  {value: 'Loss of Pay'},
+];
+employeeName = /^(?=[^\s])([a-zA-Z\d\/\- ]{3,50})$/;
+  leaveReason = /^(?=[^\s])([a-zA-Z\d\/\- ]{3,50})$/;
+  constructor(private fb: FormBuilder,
+  private Service: staffLeavesService,
+  private staffService: staffService,
+  private MessageService: MessageService,
+  private activeRoute: ActivatedRoute,
+  private router: Router,
+  ){
+    this.editLeaveForm = this.fb.group({
+      employee: ['', [Validators.required,]],
+      noOfDay: ['', [Validators.required, Validators.min(1) , Validators.max(30)]],
+      leaveType: ['', Validators.required],
+      from: ['', Validators.required],
+      to: ['', Validators.required],
+      leaveReason: ['', [Validators.required, Validators.min(3), Validators.max(50)]]
+  })
+    this.leaveId = this.activeRoute.snapshot.params["id"];
+
+  }
+  ngOnInit(): void {
+    this.staffService.getStaffData().subscribe((resp:any) => {
+      this.employeeList = resp.map(e => ({ 
+        name: `${e.firstName} ${e.lastName}`,
+        _id: {
+          _id: e._id,
+          name: `${e.firstName} ${e.lastName}`
+        }
+      }));
+      console.log(this.employeeList);
+      
+    });
+
+
+    this.Service.getLeaveDataById(this.leaveId).subscribe((resp: any) => {
+      this.leaveDataById = resp;
+      console.log("resp dta", resp);
+      this.patchForm(resp);
+    })}
+  patchForm(data) {
+    this.editLeaveForm.patchValue({
+      employee: data.employee,
+      noOfDay: data.noOfDay,
+      leaveType: data.leaveType,
+      from: data.from,
+      to: data.to,
+      leaveReason: data.leaveReason      
+    });
+  }
+
+editLeaveFormSubmit(){
+   const formData = this.editLeaveForm.value;
+  console.log(this.editLeaveForm.value);
+  const payload = {
+    employee: formData.employee,
+    from: formData.from,
+    leaveType: formData.leaveType,
+    to: formData.to,
+    noOfDay: formData.noOfDay,
+    leaveReason: formData.leaveReason,
+    id: ""
+  };
+  console.log("valid form");
+  console.log(payload);
+  if(this.editLeaveForm.valid){
+    payload.id = this.leaveId
+  this.Service.updateLeaveData(payload).subscribe((resp: any) => {
+    if (resp) {
+      if (resp.status === "success") {
+        const message = "Leave Request has been updated";
+        this.MessageService.add({ severity: "success", detail: message });
+        setTimeout(() => {
+          this.router.navigate(["/staff-leaves"]);
+        }, 400);
+      } else {
+        const message = resp.message;
+        this.MessageService.add({ severity: "error", detail: message });
+      }
+    }
+  })
+}
+}
 
 }
--- src/app/core/staffModule/staff-leaves/staff-leaves-list/staff-leaves-list.component.ts
+++ src/app/core/staffModule/staff-leaves/staff-leaves-list/staff-leaves-list.component.ts
@@ -1,12 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataService } from 'src/app/shared/data/data.service';
+import { pageSelection, apiResultFormat, staffleave } from 'src/app/shared/models/models';
+import { routes } from 'src/app/shared/routes/routes';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { staffLeavesService } from '../staff-leaves.service';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-staff-leaves-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    SharedModule,
+     DropdownModule,
+     CalendarModule
+     , ToastModule,
+      DialogModule,
+      TabViewModule,
+      ],
   templateUrl: './staff-leaves-list.component.html',
-  styleUrl: './staff-leaves-list.component.scss'
+  styleUrl: './staff-leaves-list.component.scss',
+  providers: [MessageService]
 })
 export class StaffLeavesListComponent {
+
+  public routes = routes;
+  // public staffData= [];
+  public searchDataValue = '';
+  leaveId: any;
+  showDialoge = false;
+  selectedLeave = '';
+  modalData: any = {};
+originalData = [];
+  visible: boolean = false;
+  addTaxTotal: any;
+  LeaveData = []  
+
+
+  constructor(
+    private messageService: MessageService,
+    private router: Router,
+    public dialog: MatDialog,
+    private service: staffLeavesService
+  ) { }
+
+
+    deleteLeave(Id: any) {
+      this.leaveId = Id;
+      this.modalData = {
+        title: "Delete",
+        messege: "Are you sure you want to delete this leave request Details",
+      };
+      this.showDialoge = true;
+    }
+    showNewDialog() {
+      this.showDialoge = true;
+    }
+    callBackModal() {
+      this.service.deleteLeaveData(this.leaveId).subscribe((resp: any) => {
+        this.messageService.add({ severity: "success", detail: resp.message });
+        this.getLeaveData();
+        this.showDialoge = false;
+      });
+    }
+    close() {
+      this.showDialoge = false;
+    }
+  
+    editLeave(id: any) {
+      this.router.navigate([`/staff-leaves/edit-staff-leaves/${id}`]);
+    }
+    getLeaveData() {
+      this.service.getLeaveData().subscribe((resp: any) => {
+        this.LeaveData = resp;
+        this.originalData = resp;
+        console.log("Leave request data", resp);
+      });
+    }
+    ngOnInit(): void {
+      this.getLeaveData();
+    }
+  
+    public searchData(value: any): void {
+      this.LeaveData = this.originalData.filter(i =>
+      i.employee.toLowerCase().includes(value.trim().toLowerCase())
+    );
+    }
+  
+    onPageChange(event) {
+      const startIndex = event.first;
+      const endIndex = startIndex + event.rows; 
+      const currentPageData = this.LeaveData.slice(startIndex, endIndex);
+    }
 
 }
