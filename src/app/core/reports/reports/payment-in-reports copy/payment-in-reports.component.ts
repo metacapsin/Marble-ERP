1a71109c367e06adb7cb2976201f3c58ab8fc61a
AdnanhussainMetacaps <adnan.hussain@metacapsinnovations.onmicrosoft.com>
2024-06-06 12:49:38 +0530

    channges on payment in reports done

--- src/app/core/reports/reports/payment-in-reports copy/payment-in-reports.component.ts
+++ src/app/core/reports/reports/payment-in-reports copy/payment-in-reports.component.ts
@@ -9,14 +9,11 @@ import { routes } from "src/app/shared/routes/routes";
   selector: "app-payment-in-reports",
   templateUrl: "./payment-in-reports.component.html"
 })
-export class PaymentInReportComponent{
+export class PaymentInReportComponent {
   public routes = routes;
   picker1: any;
-  searchDataValue = ""
-
-  
+  searchDataValue = ""  
   rangeDates: Date[] | undefined;
-
   invertoryData = [
     {
       Product: "Acer Aspire Desktop",
@@ -27,7 +24,6 @@ export class PaymentInReportComponent{
       salesPrice: "$618.00",
       currentStock: "102 pc",
       amount: "102 pc",
-      
     },
     {
       Product: "adidas Golf Shoes",
@@ -38,7 +34,6 @@ export class PaymentInReportComponent{
       salesPrice: "$56.00",
       currentStock: "13 pc",
       amount: "13 pc",
-     
     },
     {
       Product: "adidas Tennis Shoes",
@@ -49,12 +44,57 @@ export class PaymentInReportComponent{
       salesPrice: "$56.00",
       currentStock: "159 pc",
       amount: "159 pc",
-      
     },
   ];
 
+  searchByData = [
+    "Today", "YesterDay", "Last 7 Days", "This Month", "Last 3 Months", "Last 6 Months", "This Year"
+  ];
 
-  searchData(value: any){
+  onSearchByChange(event: any) {
+    const value = event.value;
+    const today = new Date();
+    let startDate, endDate = today;
 
+    switch (value) {
+      case 'Today':
+        startDate = today;
+        endDate = today;
+        break;
+      case 'YesterDay':
+        startDate = new Date(today.setDate(today.getDate() - 1));
+        endDate = startDate;
+        break;
+      case 'Last 7 Days':
+        startDate = new Date(today.setDate(today.getDate() - 7));
+        endDate = new Date();
+        break;
+      case 'This Month':
+        startDate = new Date(today.getFullYear(), today.getMonth(), 1);
+        break;
+      case 'Last 3 Months':
+        startDate = new Date(today.getFullYear(), today.getMonth() - 3, today.getDate());
+        break;
+      case 'Last 6 Months':
+        startDate = new Date(today.getFullYear(), today.getMonth() - 6, today.getDate());
+        break;
+      case 'This Year':
+        startDate = new Date(today.getFullYear(), 0, 1);
+        break;
+    }
+
+    this.rangeDates = [startDate, endDate];
+  }
+
+  searchData(value:any ){
+    
   }
 }
+
+// last 3 month=>{
+// today.getFullYear() return year = 2024;
+// today.getMonth() return this month = 5 = june ; because jan index is 0 ;
+// so 5- 3 = 2 = march;
+// today.getDate( return) today date = 6 ;
+// startdate= new Date(2024, 2, 6) = it return new date = 03/06/2024 
+// }
