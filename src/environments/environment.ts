5faca37e52fe40e8e9198e6ab4ffc2125f4b5c8e
metacapsin <154000829+metacapsin@users.noreply.github.com>
2024-06-15 12:00:37 +0530

    change url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://ec2-15-206-101-25.ap-south-1.compute.amazonaws.com:1338',
-  apiUrl: 'http://localhost:1338',
+  apiUrl: 'http://ec2-15-206-101-25.ap-south-1.compute.amazonaws.com:1338',
+  //apiUrl: 'http://localhost:1338',
 };
